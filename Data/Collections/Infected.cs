f5205cef0a4caf93208693816d3c2cf341835026
Gabriel Cesario <cesario203@outlook.com>
2020-08-22 17:00:00 -0300

    Adicionando mongodb.driver as dependencias do projeto

---
+++ Data/Collections/Infected.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace DotnetApi.Data.Collections
+{
+    public class Infected
+    {
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public GeoJson2DGeographicCoordinates Localization { get; set; }
+
+        public Infected(DateTime pBirthDate, string pGender, double latitude, double longitude)
+        {
+            this.BirthDate = pBirthDate;
+            this.Gender = pGender;
+            this.Localization = new GeoJson2DGeographicCoordinates(longitude,latitude);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -40,8 +40,6 @@ namespace DotnetApi
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
